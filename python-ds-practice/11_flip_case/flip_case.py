b703bf4684f1063811493e3fa9773c17bb029237
brenden <brendenjarias@gmail.com>
2023-07-25 22:21:53 -0700

    completed up to #16

--- python-ds-practice/11_flip_case/flip_case.py
+++ python-ds-practice/11_flip_case/flip_case.py
@@ -11,3 +11,20 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+    flipped_phrase = []
+    for char in phrase:
+        if to_swap.islower():
+            if char == to_swap:
+                flipped_phrase.append(char.upper())
+            elif char.lower() == to_swap:
+                flipped_phrase.append(char.lower())
+            else:
+                flipped_phrase.append(char)
+        elif to_swap.isupper():
+            if char == to_swap:
+                flipped_phrase.append(char.lower())
+            elif char.upper() == to_swap:
+                flipped_phrase.append(char.upper())
+            else:
+                flipped_phrase.append(char)
+    return ''.join(flipped_phrase)
